90d02276054d47bd546fc94d9073b3909cd33e6d
Ricardo Perez <ricardo.pereze.98@gmail.com>
2022-08-17 05:25:37 -0400

    loaders configurados

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
     entry: './src/index.js',
@@ -8,7 +9,7 @@ module.exports = {
         filename: 'bundel.js'
     },
     resolve: {
-        extensions: ['js', '.jsx']
+        extensions: ['.js', '.jsx']
 
     },
     mode: 'development',
@@ -28,6 +29,14 @@ module.exports = {
                         loader: 'html-loader'
                     }
                 ]
+            },
+            {
+                test : /\.s[ac]ss$/i,
+                use: [
+                    'style-loader',
+                    'css-loader',
+                    'sass-loader'
+                ]
             }
         ]
     },
@@ -35,6 +44,13 @@ module.exports = {
         new HtmlWebpackPlugin({
             template: './public/index.html',
             filename: './index.html'
-        })
-    ]
-}
+        }),
+        new MiniCssExtractPlugin({
+            filename: '[name].css'
+        })],
+        devServer:{
+            static: {directory: path.join(__dirname, 'dist')},
+            compress: true,
+            port: 3005
+        }
+    }
