6cb6214d2d95b006d778fd2afbeb64ab50405a4e
Ricardo Perez <ricardo.pereze.98@gmail.com>
2022-08-24 06:21:55 -0400

    componentes listos

--- src/pages/SendEmail.jsx
+++ src/pages/SendEmail.jsx
@@ -1,10 +1,10 @@
 import React from "react";
-import '../styles/RecoveryPassword.scss';
+import '../styles/SendEmail.scss';
 
-const RecoveryPassword = () => {
+const SendEmail = () => {
   return (
-    <div className="RecoveryPassword">
-      <div className="RecoveryPassword-container">
+    <div className="SendEmail">
+      <div className="SendEmail-container">
         <img src="./logos/logo_yard_sale.svg" alt="logo" className="logo" />
 
         <h1 className="title">Email has been sent!</h1>
@@ -27,4 +27,4 @@ const RecoveryPassword = () => {
   );
 };
 
-export default RecoveryPassword;
+export default SendEmail;
--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -4,11 +4,17 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Layout from "../containers/Layout";
 
 import Home from "../pages/Home";
+import CreateAccount from "../pages/CreateAccount";
 import Login from "../pages/Login";
-import RecoveryPassword from "../pages/RecoveryPassword";
+import MyAccount from "../pages/MyAccount";
+import NewPassword from "../pages/NewPassword";
+import PasswordRecovery from "../pages/PasswordRecovery";
 import NotFound from "../pages/NotFound";
 
 import "../styles/global.css";
+import SendEmail from "../pages/SendEmail";
+import Checkout from "../pages/Checkout";
+import Orders from "../pages/Orders";
 
 const App = () => {
   return (
@@ -17,7 +23,13 @@ const App = () => {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/login" element={<Login />} />
-          <Route path="/recovery-password" element={<RecoveryPassword />} />
+          <Route path="/password-recovery" element={<PasswordRecovery />} />
+          <Route path="/send-email" element={<SendEmail />} />
+          <Route path="/new-password" element={<NewPassword />} />
+          <Route path="/account" element={<MyAccount />} />
+          <Route path="/signup" element={<CreateAccount />} />
+          <Route path="/checkout" element={<Checkout />} />
+          <Route path="/orders" element={<Orders />} />
           <Route path="*" element={<NotFound />} />
         </Routes>
       </Layout>
